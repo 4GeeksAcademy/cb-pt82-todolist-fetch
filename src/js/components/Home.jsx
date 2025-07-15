7dc4dc2523b46a446bce80c3503346266d676321
Carlos Banks <48330194+carlostbanks@users.noreply.github.com>
2025-07-15 01:00:34 +0000

    added useEffect

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// const URI = 'https://playground.4geeks.com/todo'
+// const USERNAME = 'carlosb'
 
 //create your first component
 const Home = () => {
+	let [userName, setUserName] = useState('Dan28')
+
+	useEffect(() => {
+		createUser(userName);
+		getUsersTodoList();
+	}, [])
+
+
+	function createUser(username) {
+		fetch("https://playground.4geeks.com/todo/users/" + username, {
+			method: "POST",
+			body: JSON.stringify([]),
+			headers: {
+			"Content-Type": "application/json"
+			}
+		})
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			console.log(data) 
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+	}
+
+	function getUsersTodoList() {
+		fetch('https://playground.4geeks.com/todo/users/' + userName)
+		.then((response) => {
+			return response.json()
+		})
+		.then((data) => {
+			console.log("This is the data for carlosb:", data)
+			// different stuff
+			// do stuff this data (DISPLAY IT)
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+	}
+
+
 	return (
 		<div className="text-center">
+			<h1>Todo List with Fetch</h1>
+			<input
+				type="text"
+				placeholder="Enter your todo here..."
+			/>
+			<button>Submit</button>
+			<ul>
+				<li>Todo 1</li>
+				<li>Todo 2</li>
+				<li>Todo 3</li>
+			</ul>
             
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
 	);
 };
